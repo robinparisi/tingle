33eadd6a9d8c5c707579ba7073795d0c450e5fbc
Micah Brown <micahyb@gmail.com>
2018-03-27 12:11:59 -0500

    closes robinparisi/tingle#79 (#80)
    * closes robinparisi/tingle#79

--- src/tingle.js
+++ src/tingle.js
@@ -171,6 +171,11 @@
             this.modalBoxContent.innerHTML = "";
             this.modalBoxContent.appendChild(content);
         }
+
+        if (this.isOpen()) {
+            // check if modal is bigger than screen height
+            this.checkOverflow();
+        }
     };
 
     Modal.prototype.getContent = function() {
